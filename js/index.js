5770067ca3b9cdee9a82202229e4f36f012a70b8
Stanislav Huliaiev <101598952+GulShow@users.noreply.github.com>
2022-07-03 00:00:18 +0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// Task 1
+const name = 'Джон'
+const admin = name
+alert(admin)
+
+//Task 2
+const ourPlanet = 'Earth'
+const userName = `${name}`
+
+//Task 3
+const differentTypes = '10' + 10
+console.log(typeof differentTypes);
